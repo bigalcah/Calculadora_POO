4e470c4c8cf706dec2ac3b7080408d30a6d46549
chris <c.gajardo08@ufromail.cl>
2023-09-19 21:15:42 -0300

    Avance1.4 calculadora Christian Gajardo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Main {
+
+    private static Scanner leer = new Scanner(System.in);
+
     public static double Suma(double numero1, double numero2) {
         return numero1 + numero2;
     }
@@ -81,4 +85,144 @@ public class Main {
         double X = (C - B * Y) / A;
         return ("X = " + X + ", Y = " + Y);
     }
+
+
+
+    // Botones
+    public static void B_PerimetroCuadradoRectangulo(){
+        try {
+            System.out.println("Ingrese los lados.");
+            double LadoA = leer.nextDouble();
+            double LadoB = leer.nextDouble();
+
+            if (isMayorCero(LadoA) && isMayorCero(LadoB)){
+
+                System.out.println("El perimetro es: " + perimetroCuadradoRectangulo(LadoA, LadoB));
+
+            }else{
+                System.out.println("No se permiten lados menores o iguales a 0.");
+            }
+
+        } catch (Exception error){
+            System.out.println("Error al ingresar dato.");
+        }
+
+    }
+    public static void B_AreaCuadradoRectangulo(){
+        try {
+            System.out.println("Ingrese los lados.");
+            double LadoA = leer.nextDouble();
+            double LadoB = leer.nextDouble();
+
+            if (isMayorCero(LadoA) && isMayorCero(LadoB)){
+
+                System.out.println("El area es: " + areaCuadradoRectangulo(LadoA, LadoB));
+
+            }else{
+                System.out.println("No se permiten lados menores o iguales a 0.");
+            }
+
+        } catch (Exception error){
+            System.out.println("Error al ingresar dato.");
+        }
+
+    }
+    public static void B_AreaEsfera(){
+        try {
+            System.out.println("Ingrese el radio.");
+            double radio = leer.nextDouble();
+
+            if (isMayorCero(radio)){
+
+                System.out.println("El area es: " + areaEsfera(radio));
+
+            }else{
+                System.out.println("No se permite radio menor o igual a 0.");
+            }
+
+        } catch (Exception error){
+            System.out.println("Error al ingresar dato.");
+        }
+
+    }
+    public static void B_AreaCubo(){
+        try {
+            System.out.println("Ingres el lado.");
+            double LadoA = leer.nextDouble();
+
+            if (isMayorCero(LadoA)){
+
+                System.out.println("El area es: " + areaCubo(LadoA));
+
+            }else{
+                System.out.println("No se permiten lados menores o iguales a 0.");
+            }
+
+        } catch (Exception error){
+            System.out.println("Error al ingresar dato.");
+        }
+
+    }
+    public static void B_VolumenCubo(){
+        try {
+            System.out.println("Ingres el lado.");
+            double LadoA = leer.nextDouble();
+
+            if (isMayorCero(LadoA)){
+
+                System.out.println("El volumen es: " + volumenCubo(LadoA));
+
+            }else{
+                System.out.println("No se permiten lados menores o iguales a 0.");
+            }
+
+        } catch (Exception error){
+            System.out.println("Error al ingresar dato.");
+        }
+
+    }
+    public static void B_AreaCono(){
+        try {
+            System.out.println("Ingrese la altura seguida del radio.");
+            double altura = leer.nextDouble();
+            double radio = leer.nextDouble();
+
+            if (isMayorCero(altura) && isMayorCero(radio)){
+
+                System.out.println("El area es: " + areaCono(altura, radio));
+
+            }else{
+                System.out.println("No se permiten valores menores o iguales a 0.");
+            }
+
+        } catch (Exception error){
+            System.out.println("Error al ingresar dato.");
+        }
+
+    }
+
+    public static void B_InterseccionRectas(){
+        try {
+            System.out.println("Ingrese los valores para las rectas de la forma: \n\tAx + By = C \nPrimera recta\nA:");
+            double A = leer.nextDouble();
+            System.out.println("B:");
+            double B = leer.nextDouble();
+            System.out.println("C:");
+            double C = leer.nextDouble();
+
+            System.out.println("\nSegunda recta\nA:");
+            double D = leer.nextDouble();
+            System.out.println("B:");
+            double E = leer.nextDouble();
+            System.out.println("C:");
+            double F = leer.nextDouble();
+
+            System.out.println(interseccionRectas(A,B,C,D,E,F));
+
+        } catch (Exception error){
+            System.out.println("Error al ingresar dato.");
+        }
+
+    }
+
 }
